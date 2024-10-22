0e7fb619223715f4878486d1fa84c6605ee3dcad
Luc Talatinian <lucix@amazon.com>
2024-10-22 12:02:01 -0400

    runtime http

--- runtime/http.lua
+++ runtime/http.lua
@@ -1,3 +1,35 @@
 local module = {}
 
+module.Header = {}
+
+function module.Header:New()
+    local t = {
+        _values = {},
+    }
+    setmetatable(t, self)
+    self.__index = self
+    return t
+end
+
+function module.Header:Get(k)
+    return self._values[k]
+end
+
+function module.Header:Set(k, v)
+    self._values[k] = v
+end
+
+module.Request = {}
+
+function module.Request:New()
+    local t = {
+        Method = '',
+        Header = module.Header:New(),
+        Body   = nil,
+    }
+    setmetatable(t, self)
+    self.__index = self
+    return t
+end
+
 return module
